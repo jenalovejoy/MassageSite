9c387684ce06ce2b863cb0be8ceac6f52a5bbd48
Jena Lovejoy <jena.lovejoy@mac.com>
2020-11-12 23:51:44 -0800

    can send contact emails through emailjs

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -4,60 +4,86 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
 import { Col, Row } from "react-bootstrap";
 import Form from 'react-bootstrap/Form'
-import { useState } from "react";
+import{ init } from 'emailjs-com';
+import emailjs from 'emailjs-com';
+import React from 'react';
+init(userID);
 
-
-function ContactForm() {
-    const [validated, setValidated] = useState(false);
-
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-        }
-
-        setValidated(true);
+export default class ContactForm extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: '', 
+            email: '',
+            phone: '',
+            message: '' 
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
 
-    return (
-        <Container id="contact-form-container">
-            <h2>Header</h2>
-        <Form id="contact-form" validated={validated} onSubmit={handleSubmit}>
-            <Form.Group as={Row} controlID="formName">
-                <Form.Label column sm={2}>Name</Form.Label>
-                <Col sm={10}>
-                    <Form.Control required type="text" placeholder="Your name"/>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlID="formBasicEmail">
-                <Form.Label column sm={2}>Email</Form.Label>
-                <Col sm={10}>
-                    <Form.Control required type="email" placeholder="name@example.com"/>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlID="formPhone">
-                <Form.Label column sm={2}>Phone Number</Form.Label>
-                <Col sm={10}>
-                    <Form.Control required 
-                        type="tel"
-                        placeholder="Phone number"/>
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlID="formMessage">
-                <Form.Label column sm={2} >Notes</Form.Label>
-                <Col sm={10} rows={3}>
-                    <Form.Control required 
-                        as="textarea"
-                        rows={3}
-                        placeholder="Anything you'd like to say: questions, schedule preferences, etc."/>
-                </Col>
-            </Form.Group>
+    handleSubmit(event) {
+        let variables = {
+            client_name: this.state.name, 
+            client_email: this.state.email, 
+            client_phone: this.state.phone,
+            message: this.state.message};
 
-            <Button type="submit" variant="light">Submit</Button>
-        </Form>
-        </Container>
-    );
+        emailjs.send(serviceId, templateId, variables)
+        .then(res => {console.log("email sent successful!")})
+        .catch(err => console.error("error message: ", err));
+    };
+
+    render(){
+        return (
+            <Container id="contact-form-container">
+                <h2>Header</h2>
+            <Form id="contact-form" onSubmit={this.handleSubmit}>
+                <Form.Group as={Row} controld="formName">
+                    <Form.Label column sm={2}>Name</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control required 
+                            type="text" 
+                            placeholder="Your name"
+                            onChange={(event) => this.setState({name: event.target.value })} 
+                            value={this.state.name}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlID="formBasicEmail">
+                    <Form.Label column sm={2}>Email</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control required 
+                            type="email" 
+                            placeholder="name@example.com"
+                            onChange={(event) => this.setState({email: event.target.value })} 
+                            value={this.state.email}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlID="formPhone">
+                    <Form.Label column sm={2}>Phone Number</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control required 
+                            type="tel"
+                            placeholder="Phone number"
+                            onChange={(event) => this.setState({phone: event.target.value })} 
+                            value={this.state.phone}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlID="formMessage">
+                    <Form.Label column sm={2} >Notes</Form.Label>
+                    <Col sm={10} rows={3}>
+                        <Form.Control required 
+                            as="textarea"
+                            rows={3}
+                            placeholder="Anything you'd like to say: questions, schedule preferences, etc."
+                            onChange={(event) => this.setState({message: event.target.value })} 
+                            value={this.state.message}/>
+                    </Col>
+                </Form.Group>
+                <Button type="submit" variant="light">Submit</Button>
+            </Form>
+            </Container>
+        );
+    }
 }
 
-export default ContactForm;
+// export default ContactForm;
